e6fe215ef755b868a4f7ca49100037fa58d2deea
KcasTischaWattt <tmnbob2012@gmail.com>
2024-03-29 13:19:45 +0300

    Added NavBar

--- frontend/src/components/NavBar.vue
+++ frontend/src/components/NavBar.vue
@@ -1,24 +1,70 @@
-<script>
-</script>
-
 <template>
-    <nav>
-        <ul>
-        <li>
-            <router-link to="/">Home</router-link>
-        </li>
-        <li>
-            <router-link to="/news">News</router-link>
-        </li>
-        <li>
-            <router-link to="/search">Search</router-link>
-        </li>
-        <li>
-            <router-link to="/statistics">Statistics</router-link>
-        </li>
-        <li>
-            <router-link to="/login">Login</router-link>
-        </li>
-        </ul>
-    </nav>
+  <q-layout>
+    <q-header elevated class="bg-secondary text-white">
+      <q-toolbar class="row items-center">
+        <q-btn flat dense to="/">
+          <img
+            src="../resources/KRUT2.png"
+            alt="Website Logo"
+            style="height: 40px"
+          />
+        </q-btn>
+        <q-btn
+          flat
+          dense
+          to="/"
+          exact
+          active-class="text-white"
+          class="text-white"
+        >
+          Home
+        </q-btn>
+        <q-btn
+          flat
+          dense
+          to="/news"
+          active-class="text-white"
+          class="text-white"
+        >
+          News
+        </q-btn>
+        <q-btn
+          flat
+          dense
+          to="/search"
+          active-class="text-white"
+          class="text-white"
+        >
+          Search
+        </q-btn>
+        <q-btn
+          flat
+          dense
+          to="/statistics"
+          active-class="text-white"
+          class="text-white"
+        >
+          Statistics
+        </q-btn>
+        <q-space />
+        <div>
+          <q-btn
+            flat
+            dense
+            to="/login"
+            active-class="text-white"
+            class="text-white"
+          >
+            Login
+          </q-btn>
+        </div>
+      </q-toolbar>
+    </q-header>
+  </q-layout>
 </template>
+
+<script>
+export default {
+  name: "AppHeader",
+};
+</script>
