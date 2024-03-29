7669743a5a4fa12120ecf07d03086d53bfbb1724
KcasTischaWattt <tmnbob2012@gmail.com>
2024-03-29 15:25:48 +0300

    Added responsive design to login form

--- frontend/src/components/LoginPage.vue
+++ frontend/src/components/LoginPage.vue
@@ -2,8 +2,8 @@
   <q-layout view="lHh Lpr lFf">
     <q-page-container>
       <q-page class="flex flex-center">
-        <q-card class="q-pa-md" style="max-width: 400px">
-          <q-card-section class="text-h6">Login</q-card-section>
+        <q-card class="q-pa-md" :style="{ maxWidth: cardWidth }">
+          <q-card-section class="text-h5">Login</q-card-section>
           <q-card-section>
             <q-form @submit="login">
               <q-input
@@ -12,6 +12,7 @@
                 label="Username"
                 dense
                 class="q-mb-md"
+                style="font-size: 18px; height: 48px"
               />
               <q-input
                 v-model="password"
@@ -20,6 +21,7 @@
                 type="password"
                 dense
                 class="q-mb-md"
+                style="font-size: 18px; height: 48px"
               />
               <!--TODO Add functionality-->
               <q-btn
@@ -29,6 +31,7 @@
                 dense
                 to="/"
                 class="full-width"
+                style="font-size: 18px; height: 40px"
               />
             </q-form>
           </q-card-section>
@@ -44,7 +47,15 @@ export default {
     return {
       username: "",
       password: "",
+      cardWidth: "400px",
     };
+  },
+  mounted() {
+    this.calculateCardWidth();
+    window.addEventListener("resize", this.calculateCardWidth);
+  },
+  beforeUnmount() {
+    window.removeEventListener("resize", this.calculateCardWidth);
   },
   methods: {
     async login() {
@@ -67,6 +78,14 @@ export default {
         }
       } catch (error) {
         console.error("Error during login:", error);
+      }
+    },
+    calculateCardWidth() {
+      const screenWidth = window.innerWidth;
+      if (screenWidth >= 600) {
+        this.cardWidth = "400px";
+      } else {
+        this.cardWidth = "90%";
       }
     },
   },
