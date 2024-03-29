4b5a65fe9e90221811867ce16fa46f743c899614
KcasTischaWattt <tmnbob2012@gmail.com>
2024-03-29 17:23:46 +0300

    Upd some views

--- frontend/src/views/SearchView.vue
+++ frontend/src/views/SearchView.vue
@@ -1,9 +1,17 @@
 <script setup>
 import SearchPage from "../components/SearchPage.vue";
+import NavBar from "../components/NavBar.vue";
+import FooterBar from "../components/FooterBar.vue";
 </script>
 
 <template>
-  <div>
+  <q-layout view="lHh LpR lFf">
+    <q-header>
+      <NavBar />
+    </q-header>
     <SearchPage />
-  </div>
+    <q-footer>
+      <FooterBar />
+    </q-footer>
+  </q-layout>
 </template>
--- frontend/src/views/StatisticsView.vue
+++ frontend/src/views/StatisticsView.vue
@@ -1,10 +1,17 @@
 <script setup>
-import StatisticsPage from '../components/StatisticsPage.vue';
+import StatisticsPage from "../components/StatisticsPage.vue";
+import NavBar from "../components/NavBar.vue";
+import FooterBar from "../components/FooterBar.vue";
 </script>
 
 <template>
-    <div>
-        Hello
-        <StatisticsPage />
-    </div>
+  <q-layout view="lHh LpR lFf">
+    <q-header>
+      <NavBar />
+    </q-header>
+    <StatisticsPage />
+    <q-footer>
+      <FooterBar />
+    </q-footer>
+  </q-layout>
 </template>
