0cd3f96e540a1e245922b16d5983f714511027bf
CehhGhost <ghost5016628@mail.ru>
2024-03-29 23:21:08 +0300

    added method to get users

--- backend/src/main/java/spring/crut/administration/controllers/AdminController.java
+++ backend/src/main/java/spring/crut/administration/controllers/AdminController.java
@@ -12,6 +12,8 @@ import spring.crut.administration.models.Role;
 import spring.crut.administration.models.User;
 import spring.crut.administration.services.UsersService;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Map;
 
 @RestController
@@ -38,5 +40,13 @@ public class AdminController {
     public ResponseEntity<HttpStatus> changePasswordForUser(@PathVariable Integer id, @RequestBody Map<String, String> requestBody) {
         usersService.changePasswordForUser(id, requestBody.get("oldPassword"), requestBody.get("newPassword"));
         return ResponseEntity.ok(HttpStatus.OK);
+    }
+    @GetMapping("/user")
+    public ResponseEntity<?> getAllUsers() {
+        List<UserDTO> usersDTO = new ArrayList<>();
+        for (var user : usersService.getAllUsers()) {
+            usersDTO.add(modelMapper.map(user, UserDTO.class));
+        }
+        return ResponseEntity.ok(usersDTO);
     }
 }
