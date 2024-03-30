82133176ddf66573c4cc13fae4e7db9e0f9e538c
KcasTischaWattt <tmnbob2012@gmail.com>
2024-03-30 13:18:32 +0300

    Added Recogito JS

---
+++ frontend/src/components/DocumentDemo.vue
@@ -0,0 +1,59 @@
+<script>
+import { Recogito } from "@recogito/recogito-js";
+import "@recogito/recogito-js/dist/recogito.min.css";
+
+export default {
+  data() {
+    return {
+      recogito: null,
+    };
+  },
+  mounted() {
+    this.recogito = new Recogito({
+      content: "recogito",
+      mode: "annotate",
+      selectHandler: (annotation) => {
+        console.log("Selected:", annotation);
+      },
+      createAnnotationHandler: (annotation) => {
+        console.log("Created:", annotation);
+      },
+      updateAnnotationHandler: (annotation) => {
+        console.log("Updated:", annotation);
+      },
+      deleteAnnotationHandler: (annotation) => {
+        console.log("Deleted:", annotation);
+      },
+    });
+  },
+  beforeUnmount() {
+    if (this.recogito) {
+      this.recogito.destroy();
+    }
+  },
+};
+</script>
+
+<template>
+  <div>
+    <div ref="recogitoContainer" style="height: 500px" id="recogito">
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam viverra
+        urna vitae orci sagittis, ac tempus ligula ultrices. Mauris non ipsum eu
+        lectus vehicula vulputate id ac tortor. In et ligula in libero vulputate
+        egestas. Fusce rutrum non risus nec feugiat. Integer quis lacus
+        malesuada, congue dolor sit amet, condimentum libero.
+      </p>
+
+      <p>
+        Donec tincidunt urna libero, sit amet fermentum justo auctor vel. Ut
+        sodales lorem id semper maximus. Fusce dapibus arcu quis justo ultrices,
+        et consequat velit placerat. Pellentesque habitant morbi tristique
+        senectus et netus et malesuada fames ac turpis egestas. Proin feugiat
+        nunc a turpis ullamcorper molestie. Praesent vitae sollicitudin libero,
+        nec molestie odio. Suspendisse potenti. Vivamus non tincidunt libero.
+        Cras non dui mi.
+      </p>
+    </div>
+  </div>
+</template>
---
+++ frontend/src/views/DemoView.vue
@@ -0,0 +1,17 @@
+<script setup>
+import DocumentDemo from "../components/DocumentDemo.vue";
+import NavBar from "../components/NavBar.vue";
+import FooterBar from "../components/FooterBar.vue";
+</script>
+
+<template>
+  <q-layout view="lHh LpR lFf">
+    <q-header>
+      <NavBar />
+    </q-header>
+    <DocumentDemo />
+    <q-footer>
+      <FooterBar />
+    </q-footer>
+  </q-layout>
+</template>
