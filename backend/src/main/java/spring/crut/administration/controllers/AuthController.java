9b8cb6bc3482c58a85a0277a7cd2717dc563d679
CehhGhost <ghost5016628@mail.ru>
2024-04-01 16:08:43 +0300

    added method to validate JWT

--- backend/src/main/java/spring/crut/administration/controllers/AuthController.java
+++ backend/src/main/java/spring/crut/administration/controllers/AuthController.java
@@ -1,5 +1,6 @@
 package spring.crut.administration.controllers;
 
+import com.auth0.jwt.exceptions.JWTVerificationException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -7,15 +8,14 @@ import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import spring.crut.administration.dto.AuthDTO;
 import spring.crut.administration.dto.JwtDTO;
 import spring.crut.administration.dto.UserDTO;
 import spring.crut.administration.repositories.UsersRepository;
 import spring.crut.administration.security.JWTUtil;
+
+import javax.servlet.http.HttpServletResponse;
 
 @RestController
 @RequiredArgsConstructor
@@ -23,6 +23,7 @@ import spring.crut.administration.security.JWTUtil;
 public class AuthController {
     private final AuthenticationManager authenticationManager;
     private final JWTUtil jwtUtil;
+
     @PostMapping
     public ResponseEntity<?> loginUser(@RequestBody AuthDTO authDTO) {
         UsernamePasswordAuthenticationToken authInputToken = new UsernamePasswordAuthenticationToken(authDTO.getUsername(), authDTO.getPassword());
@@ -34,5 +35,13 @@ public class AuthController {
         String jwt = jwtUtil.generateToken(authDTO.getUsername());
         JwtDTO jwtDTO = new JwtDTO(jwt);
         return ResponseEntity.ok(jwtDTO);
+    }
+    @GetMapping("/check_login") ResponseEntity<?> checkValidationOfToken(@RequestParam(name = "jwt") String jwt) {
+        try {
+            jwtUtil.validateTokenAndRetrieveClaim(jwt);
+        } catch (JWTVerificationException exc) {
+            return ResponseEntity.badRequest().build();
+        }
+        return ResponseEntity.ok(HttpStatus.OK);
     }
 }
