a33cf5ccdd7483c878f67cff0b9cbd68f8af92bb
CehhGhost <ghost5016628@mail.ru>
2024-04-02 14:57:21 +0300

    updated so documents status now show up with integer

--- backend/src/main/java/spring/crut/corpus/controllers/DocumentsController.java
+++ backend/src/main/java/spring/crut/corpus/controllers/DocumentsController.java
@@ -11,6 +11,7 @@ import spring.crut.administration.security.CrutUserDetails;
 import spring.crut.corpus.dto.CreateDocumentDTO;
 import spring.crut.corpus.dto.CertainSearchDTO;
 import spring.crut.corpus.dto.DocumentDTO;
+import spring.crut.corpus.enums.Status;
 import spring.crut.corpus.models.Document;
 import spring.crut.corpus.services.DocumentsService;
 import spring.crut.corpus.services.SentencesService;
@@ -53,6 +54,7 @@ public class DocumentsController {
         for (var document: documentsService.getAllDocuments()) {
             var documentDTO = modelMapper.map(document, DocumentDTO.class);
             documentDTO.setOwnerUsername(document.getOwner().getUsername());
+            documentDTO.setStatusNum(Status.valueOf(document.getStatus().name()).ordinal());
             documentsService.setAttrsForTokensInDocumentDTO(documentDTO);
             documentsDTO.add(documentDTO);
         }
