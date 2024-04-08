d2648f7a61b9dda9bb2393e806bf5454d35d99c0
KcasTischaWattt <tmnbob2012@gmail.com>
2024-04-08 18:10:46 +0300

    Styled document page

--- frontend/src/components/DocumentPage.vue
+++ frontend/src/components/DocumentPage.vue
@@ -1,14 +1,13 @@
 <template>
   <q-page-container>
     <h3></h3>
-    <q-page class="q-pa-md" style="max-width: 1000px; margin: 0 auto">
+    <q-page class="q-pa-xs" style="max-width: 1000px; margin: 0 auto">
       <div v-if="responseSuccess">
-        <div class="row">
-          <h3 class="row text-h6 q-mb-md">{{ document.title }}</h3>
-        </div>
-        <q-card class="rounded-borders q-mb-md">
+        <h3 class="row q-mb-md">{{ document.title }}</h3>
+        <div class="row"></div>
+        <q-card class="rounded-borders q-mb-xs">
           <q-card-section class="row items-center">
-            <h2>{{ document.text }}</h2>
+            <div>{{ document.text }}</div>
           </q-card-section>
         </q-card>
       </div>
