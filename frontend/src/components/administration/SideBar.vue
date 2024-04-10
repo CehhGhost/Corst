cac60e285bad58df8bbe99a31952c2226be280c7
KcasTischaWattt <tmnbob2012@gmail.com>
2024-04-10 14:08:41 +0300

    Styled sidebar

--- frontend/src/components/administration/SideBar.vue
+++ frontend/src/components/administration/SideBar.vue
@@ -7,22 +7,50 @@
       :overlay="true"
       :persistent="persistent"
     >
-      <q-list no-border>
-        <q-item
-          v-for="(route, index) in routes"
-          :key="index"
-          clickable
-          @click="navigateTo(route.path)"
-          :active-class="'bg-grey-2'"
+      <q-banner class="absolute-top bg-primary" style="height: 130px">
+        <div
+          class="absolute-bottom bg-transparent d-flex flex-column justify-center items-center"
         >
-          <q-item-section>
-            <q-item-label
-              style="color: #18bc9c; font-weight: 500; font-size: 18px"
-              >{{ route.label }}</q-item-label
-            >
-          </q-item-section>
-        </q-item>
-      </q-list>
+          <div class="d-flex justify-center w-100">
+            <img
+              src="../../resources/KRUT2.png"
+              alt="Website Logo"
+              style="height: 80px; margin-left: 25%"
+            />
+          </div>
+          <div
+            class="text-center text-white"
+            style="font-size: 18px; margin-bottom: 10px"
+          >
+            Matvey Tataurovskiy
+          </div>
+        </div>
+      </q-banner>
+
+      <q-scroll-area
+        style="
+          height: calc(100% - 130px);
+          margin-top: 130px;
+          border-right: 1px solid #ccc;
+        "
+      >
+        <q-list>
+          <q-item
+            v-for="(route, index) in routes"
+            :key="index"
+            clickable
+            @click="navigateTo(route.path)"
+            :active-class="'bg-grey-2'"
+          >
+            <q-item-section>
+              <q-item-label
+                style="color: #18bc9c; font-weight: 350; font-size: 18px"
+                >{{ route.label }}</q-item-label
+              >
+            </q-item-section>
+          </q-item>
+        </q-list>
+      </q-scroll-area>
     </q-drawer>
   </q-page-container>
 </template>
--- frontend/src/views/administration/AdminMainView.vue
+++ frontend/src/views/administration/AdminMainView.vue
@@ -1,9 +1,11 @@
 <script setup>
 import SideBar from "src/components/administration/SideBar.vue";
+import NavBar from "src/components/NavBar.vue";
+import FooterBar from "src/components/FooterBar.vue";
 </script>
 
 <template>
-  <q-layout view="lhr Lpr lfr">
+  <q-layout view="lHh Lpr lff">
     <SideBar />
     <div class="ml-64">
       <router-view />
