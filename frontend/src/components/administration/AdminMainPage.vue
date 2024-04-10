4eedd8afce0ee22f15cddd54205a9a0b93436605
KcasTischaWattt <tmnbob2012@gmail.com>
2024-04-10 15:35:50 +0300

    Vue JS moment

---
+++ frontend/src/components/administration/AdminMainPage.vue
@@ -0,0 +1,17 @@
+<template>
+  <q-page-container>
+    <q-page class="flex flex-center">
+      <q-card class="q-pa-md" style="max-width: 400px">
+        <q-card-section class="text-h6 text-center">
+          Welcome to CoRST administration page!
+        </q-card-section>
+      </q-card>
+    </q-page>
+  </q-page-container>
+</template>
+
+<script>
+export default {
+  name: "AdminMainPage",
+};
+</script>
