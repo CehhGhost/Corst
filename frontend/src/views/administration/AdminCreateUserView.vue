59674ed3e5bcd585280054331d3afad4828de6c1
KcasTischaWattt <tmnbob2012@gmail.com>
2024-04-11 13:32:24 +0300

    Added CreateUserPage

---
+++ frontend/src/views/administration/AdminCreateUserView.vue
@@ -0,0 +1,17 @@
+<script setup>
+import NavBar from "src/components/administration/NavBar.vue";
+import SideBar from "src/components/administration/SideBar.vue";
+import AdminCreateUser from "src/components/administration/CreateUserPage.vue";
+</script>
+
+<template>
+  <q-layout view="lHh Lpr lff">
+    <q-header>
+      <NavBar />
+    </q-header>
+    <SideBar />
+    <div>
+      <AdminCreateUser />
+    </div>
+  </q-layout>
+</template>
