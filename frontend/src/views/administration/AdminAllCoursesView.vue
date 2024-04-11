f9fe237f4e225f9429f604a1bccfc372762c1b0d
KcasTischaWattt <tmnbob2012@gmail.com>
2024-04-11 20:46:02 +0300

    Added main administration pages for document info

---
+++ frontend/src/views/administration/AdminAllCoursesView.vue
@@ -0,0 +1,17 @@
+<script setup>
+import NavBar from "src/components/administration/NavBar.vue";
+import SideBar from "src/components/administration/SideBar.vue";
+import AdminAllCourses from "src/components/administration/AllCoursesPage.vue";
+</script>
+
+<template>
+  <q-layout view="lHh Lpr lff">
+    <q-header>
+      <NavBar />
+    </q-header>
+    <SideBar />
+    <div>
+      <AdminAllCourses />
+    </div>
+  </q-layout>
+</template>
