a818f6bd4986b209b3668e29b3f69661e8de2581
CehhGhost <ghost5016628@mail.ru>
2024-08-14 15:29:28 +0300

    now you can see document by sentence id

--- backend/src/main/java/spring/crut/corpus/controllers/DocumentsController.java
+++ backend/src/main/java/spring/crut/corpus/controllers/DocumentsController.java
@@ -119,4 +119,8 @@ public class DocumentsController {
     public ResponseEntity<?> getContextForSentence(@PathVariable Long id, @PathVariable Integer amount) {
         return ResponseEntity.ok(sentencesService.getContextForSentence(id, amount));
     }
+    @GetMapping("/get_by_sentence/{id}")
+    public ResponseEntity<?> getDocumentBySentence(@PathVariable Long id) {
+        return this.getDocumentByID(sentencesService.getDocumentIdBySentence(id));
+    }
 }
